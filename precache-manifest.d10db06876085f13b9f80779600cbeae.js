87dc28daf620d02664e69796936f6750980b28a6
Calebm5577 <calebm5577@gmail.com>
2020-06-20 17:54:51 -0400

    Updates

---
+++ precache-manifest.d10db06876085f13b9f80779600cbeae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e449f58b4192fa56360109595f1af0",
+    "url": "/Movie-Search-APP%20/index.html"
+  },
+  {
+    "revision": "9b876312f50e0e0562ce",
+    "url": "/Movie-Search-APP%20/static/css/main.7cde1606.chunk.css"
+  },
+  {
+    "revision": "f078c567f8fe9ed27895",
+    "url": "/Movie-Search-APP%20/static/js/2.8da7c87b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search-APP%20/static/js/2.8da7c87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b876312f50e0e0562ce",
+    "url": "/Movie-Search-APP%20/static/js/main.9314ecf8.chunk.js"
+  },
+  {
+    "revision": "6e38221e4deeadf847e6",
+    "url": "/Movie-Search-APP%20/static/js/runtime-main.d039cf27.js"
+  }
+]);
